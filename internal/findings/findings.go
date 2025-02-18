d1625f47ea80de68232181dad4566507260dc430
lacework-mderrer <125611603+lacework-mderrer@users.noreply.github.com>
2025-02-18 11:00:22 -0800

    Update findings.go
    Added  TtpComposite const for supporting composite alerts in mappings.go

--- internal/findings/findings.go
+++ internal/findings/findings.go
@@ -27,6 +27,7 @@ const (
 	SoftwareCVE           = "Software and Configuration Checks/Vulnerabilities/CVE"
 	SoftwarePolicy        = "Software and Configuration Checks/Policy"
 	AWSCompliance         = "Software and Configuration Checks/Industry and Regulatory Standards/CIS AWS Foundations Benchmark"
+	TtpComposite          = "TTPs/Unusual Behaviors"
 
 	ArnFormat = "arn:aws:securityhub:%s::product/lacework/lacework"
 
